38706958e95062cca5a2052fa67b98d675bf08aa
Eric <eric.vasquez3327@gmail.com>
2022-07-18 12:55:46 -0500

    update

--- server.js
+++ server.js
@@ -6,6 +6,25 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+//Added
+const path = require("path")
+require("dotenv").config()
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 require('./server/routes/patient.routes')(app)
+
+//Step 1
+/* app.use(express.static(path.resolve(__dirname, "./client/build"))); */
+//Step 2
+/* app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  }); */
+
 const port = 8000;
+
+//Added
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 app.listen(port, () => console.log(`GIC db app listening on port ${port}`))
--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/gic_patient_db", { 
+// mongoose.connect("mongodb://localhost/gic_patient_db", { 
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// })
+
+//Added
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });	
+	
+/* mongoose.connect("mongodb+srv://user13:user13@cluster0.udwtc.mongodb.net/myFirstDatabase" , { 
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
-})
+}) */
 	.then(() => console.log("Connected to the gic_patient_db")) 
 	.catch(err => console.log("Something went wrong when connecting to the gic_patient_db", err));
